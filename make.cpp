3c3a2ed96dc2d4de1c235f8e5eb37e5de224f01f
Pocky <chen-gz@outlook.com>
2019-04-26 00:09:54 +0800

    修改elm文件全局变量和对函数重载

--- make.cpp
+++ make.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
+#include<time.h>
+#include "facedetectcnn.h"
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,19 +13,21 @@
 #include <opencv2/highgui/highgui_c.h>
 #include<opencv2/opencv.hpp>  
 
-#include <fstream>
-#include <string>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_transforms.h>
+#include <dlib/image_io.h>
+#include <dlib/opencv.h>
+
 #include <Eigen/Dense>
 #include <Eigen/SVD>
-#include<opencv2/core/eigen.hpp>
 #include <dirent.h>
 
-#include <algorithm>    // std::sort
-#include <vector>       // std::vector
-
-#include <stdio.h>
-#include<pthread.h>
-#include<math.h>
+#include <fstream>
+#include <iostream>
+#include <opencv2/core/eigen.hpp>
+#include <string>
 
 #include "test.hpp"
 #include "facedetectcnn.h"
