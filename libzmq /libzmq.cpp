cdc5dca7ad3c00ad3ff4257f5dac8035bc63cf32
Pocky <chen-gz@outlook.com>
2019-04-26 09:51:42 +0800

    添加libzmq
    将新文件夹的build ignore
    libzmq有函数 还有环境

---
+++ libzmq /libzmq.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <opencv2/opencv.hpp>
+#include <unistd.h>
+#include <zmq.hpp>
+using namespace std;
+
+void send_msg(zmq::socket_t &socket, std::string str) {
+    zmq::message_t msg(str.size());
+    memcpy(msg.data(), str.c_str(), str.size());
+    socket.send(msg);
+}
+std::string recv_msg(zmq::socket_t &socket) {
+    zmq::message_t received;
+    socket.recv(&received);
+    return std::string((char *)received.data(), received.size());
+}
+void send_pic(zmq::socket_t &socket, cv::Mat img) {
+    // cv::Mat img = cv::imread("../pic/1.png");
+    std::vector<uchar> img_data;
+    cv::imencode(".png", img, img_data);
+    zmq::message_t pic(img_data.size());
+    zmq::message_t msg(img_data.size());
+    memcpy(pic.data(), img_data.data(), img_data.size());
+    socket.send(pic);
+}
+void send_pic(zmq::socket_t &socket, std::string path) {
+    cv::Mat img = cv::imread(path);
+    send_pic(socket, img);
+}
