94971870934b9d879020019e888179ddb4357530
huoda <1997huoda@gmail.com>
2019-05-02 16:21:16 +0800

    突如其来的原地爆炸
    其他所有功能都能用 就是 训练模型按下去 能进去训练函数 能改变数字标签 能提取特征 就是不能改变w b beta 参数

--- make.cpp
+++ make.cpp
@@ -3,11 +3,12 @@
 // {
 //  return 0;
 // }
-
+// extern int model_num;
+MatrixXd W[5], b[5], beta[5];
 int main(){
 	eve_init();
 	// main 函数变量		MatrixXd
-	MatrixXd W[model_num], b[model_num], beta[model_num];
+	// MatrixXd W[model_num], b[model_num], beta[model_num];
 	//应该首先开机训练 50人 训练4s 写参数txt 14s	就不需要读取参数了
 	//也就不用写参数了
 	train_elm(W, b, beta);
