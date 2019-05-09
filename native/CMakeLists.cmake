0dd65f0ed2cdb266cd5464fc443232ceff11d717
huoda <1997huoda@gmail.com>
2019-05-10 02:37:37 +0800

    NEON

---
+++ native/CMakeLists.cmake
@@ -0,0 +1,35 @@
+project(pico)
+cmake_minimum_required(VERSION 2.8)
+
+set(CMAKE_CXX_STANDARD 11)
+find_package(dlib REQUIRED)
+cmake_policy(SET CMP0028 OLD)
+# find_package(OpenCV REQUIRED)
+# find_package(OpenMP)
+# if(OPENMP_FOUND)
+#     message("OPENMP FOUND")
+#     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+#     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_C_FLAGS}")
+#     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+# endif()
+SET(CMAKE_CXX_FLAGS_RELEASE "$ENV{CXXFLAGS} -O3 -Wall")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -pthread -O3")
+add_definitions(-fPIC  -fopenmp)
+
+include_directories(../src)
+include_directories(../native)
+# include_directories(..)
+include_directories(${OpenCV_INCLUDE_DIRS})
+
+include(../libYu.cmake)
+
+link_directories(${OpenCV_LIBRARY_DIRS})
+
+file(GLOB SRC ../native/*.cpp)
+# add_subdirectory(../native/dlib dlib_build)
+
+#add_executable(pico ${SRC})
+add_library(libpico SHARED ${SRC})
+
+target_link_libraries(libpico libfacedetection dlib::dlib ${OpenCV_LIBS})
+
