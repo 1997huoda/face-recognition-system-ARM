0b1ccf92b8e5419d73e0f491d574309128b26575
chen-gz <chen-gz@outlook.com>
2019-05-10 15:18:19 +0800

    .

--- align/align.hpp
+++ align/align.hpp
@@ -1,14 +1,9 @@
 #ifndef _ALIGN_H
 #define _ALIGN_H
 
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/objdetect.hpp>
+
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
-#include <opencv2/highgui/highgui_c.h>
 #include <opencv2/opencv.hpp>
 
 #include <dlib/image_processing.h>
