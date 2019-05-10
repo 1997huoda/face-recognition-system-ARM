4d69ccc7a1e24048943bc8608e76a6ba7a1b0853
huoda <1997huoda@gmail.com>
2019-05-10 18:00:17 +0800

    ..

--- zmq/server.cpp
+++ zmq/server.cpp
@@ -3,20 +3,20 @@
 #include <unistd.h>	
 #include <zmq.hpp>	
 using namespace std;
-void send_msg(zmq::socket_t &socket, std::string str) {	
-    zmq::message_t msg(str.size());	
-    memcpy(msg.data(), str.c_str(), str.size());	
-    socket.send(msg);	
-}	
-std::string recv_msg(zmq::socket_t &socket) {	
-    zmq::message_t received;	
-    socket.recv(&received);	
-    return std::string((char *)received.data(), received.size());	
-}
-void send_pic(zmq::socket_t &socket, std::string path) {	
-    cv::Mat img = cv::imread(path);	
-    send_pic(socket, img);	
-}
+// void send_msg(zmq::socket_t &socket, std::string str) {	
+//     zmq::message_t msg(str.size());	
+//     memcpy(msg.data(), str.c_str(), str.size());	
+//     socket.send(msg);	
+// }	
+// std::string recv_msg(zmq::socket_t &socket) {	
+//     zmq::message_t received;	
+//     socket.recv(&received);	
+//     return std::string((char *)received.data(), received.size());	
+// }
+// void send_pic(zmq::socket_t &socket, std::string path) {	
+//     cv::Mat img = cv::imread(path);	
+//     send_pic(socket, img);	
+// }
 int main() {	
 
 	std::string command="send_picture";	
@@ -71,7 +71,8 @@ int main() {
             //收	
 //             socket.recv(&request);	
             //发送图片	
-            send_pic(socket, "../none.bmp");	
+            Mat send_to=imread("../none.bmp");
+            send_pic(socket,send_to);	
             //收	
             socket.recv(&request);	
         } else {	
