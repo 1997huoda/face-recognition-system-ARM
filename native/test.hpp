ad39f1ce6463bf3ee301a5d4e6dce01727d9ead1
chen-gz <chen-gz@outlook.com>
2019-05-10 15:24:55 +0800

    .

--- native/test.hpp
+++ native/test.hpp
@@ -9,8 +9,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/objdetect.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
 #include <opencv2/highgui/highgui_c.h>
