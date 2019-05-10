1cda4297745fbf2d8bfb8ded27c78f2769c37617
huoda <1997huoda@gmail.com>
2019-05-10 17:58:04 +0800

    .

--- zmq/server.cpp
+++ zmq/server.cpp
@@ -3,6 +3,16 @@
 #include <unistd.h>	
 #include <zmq.hpp>	
 using namespace std;
+void send_msg(zmq::socket_t &socket, std::string str) {	
+    zmq::message_t msg(str.size());	
+    memcpy(msg.data(), str.c_str(), str.size());	
+    socket.send(msg);	
+}	
+std::string recv_msg(zmq::socket_t &socket) {	
+    zmq::message_t received;	
+    socket.recv(&received);	
+    return std::string((char *)received.data(), received.size());	
+}
 void send_pic(zmq::socket_t &socket, std::string path) {	
     cv::Mat img = cv::imread(path);	
     send_pic(socket, img);	
