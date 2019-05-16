6e6d29dcf439934b991b9e2241ba6435a83fd55b
chen-gz <chen-gz@outlook.com>
2019-05-16 22:08:45 +0800

    add gamma in aligncpp

--- align/align.cpp
+++ align/align.cpp
@@ -12,7 +12,45 @@ float getticks()
 }
 dlib::shape_predictor sp;               // dlib shape predictor
 vector<Mat> alignment_face_recall;
-
+void MyGammaCorrection(Mat& src, Mat& dst, float fGamma)
+{
+	CV_Assert(src.data);
+	// accept only char type matrices
+	CV_Assert(src.depth() != sizeof(uchar));
+	// build look up table
+	unsigned char lut[256];
+	for( int i = 0; i < 256; i++ )
+	{
+		lut[i] = saturate_cast<uchar>(pow((float)(i/255.0), fGamma) * 255.0f);
+	}
+	dst = src.clone();
+	const int channels = dst.channels();
+	switch(channels)
+	{
+		case 1:
+			{
+				MatIterator_<uchar> it, end;
+				for( it = dst.begin<uchar>(), end = dst.end<uchar>(); it != end; it++ )
+					//*it = pow((float)(((*it))/255.0), fGamma) * 255.0;
+					*it = lut[(*it)];
+				break;
+			}
+		case 3: 
+			{
+				MatIterator_<Vec3b> it, end;
+				for( it = dst.begin<Vec3b>(), end = dst.end<Vec3b>(); it != end; it++ )
+				{
+					//(*it)[0] = pow((float)(((*it)[0])/255.0), fGamma) * 255.0;
+					//(*it)[1] = pow((float)(((*it)[1])/255.0), fGamma) * 255.0;
+					//(*it)[2] = pow((float)(((*it)[2])/255.0), fGamma) * 255.0;
+					(*it)[0] = lut[((*it)[0])];
+					(*it)[1] = lut[((*it)[1])];
+					(*it)[2] = lut[((*it)[2])];
+				}
+				break;
+			}
+	}
+}
 void face_alignment(Mat image_roi){
 	//equalizeHist
 	cv::cvtColor(image_roi, image_roi, CV_BGR2GRAY);
